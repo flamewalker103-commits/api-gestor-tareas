7f40c0236a20a0e895ee5224ce6466ff57b6ec55
flamewalker103-commits <ouchalesho@hotmail.com>
2025-11-08 07:08:03 -0600

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+let tareas = [];
+
+app.post('/tareas', (req, res) => {
+  const { id, descripcion } = req.body;
+  tareas.push({ id, descripcion });
+  res.status(201).send({ id, descripcion });
+});
+
+app.get('/tareas', (req, res) => {
+  res.send(tareas);
+});
+
+app.put('/tareas/:id', (req, res) => {
+  const { id } = req.params;
+  const { descripcion } = req.body;
+  const tareaIndex = tareas.findIndex(t => t.id === id);
+  if (tareaIndex !== -1) {
+    tareas[tareaIndex].descripcion = descripcion;
+    res.send(tareas[tareaIndex]);
+  } else {
+    res.status(404).send({ error: 'Tarea no encontrada' });
+  }
+});
+
+app.delete('/tareas/:id', (req, res) => {
+  const { id } = req.params;
+  tareas = tareas.filter(t => t.id !== id);
+  res.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`API escuchando en http://localhost:${port}`);
+});
